17b241f9b19f1a4a39086bd400a91b5a34244781
AASHISHK4568 <aashishk4568@gmail.com>
2025-04-12 19:03:54 +0530

    logout feature + better error message in case of wrong id,pass: /login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,36 +6,39 @@ import { useNavigate } from 'react-router-dom';
 import { BASE_URL } from '../utils/constants';
 
 const Login = () => {
+  const [emailId, setEmailId] = useState('rahul@gmail.com');
+  const [password, setPassword] = useState('mSd123@#');
+  const [error, setError] = useState('');
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    const [emailId, setEmailId] = useState("rahul@gmail.com");
-    const [password, setPassword] = useState("mSd123@#");
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(
+        BASE_URL + '/login',
+        {
+          emailId,
+          password,
+        },
+        { withCredentials: true }
+      );
 
-    const handleLogin = async () => {
-        try{
-            const res = await axios.post(BASE_URL +"/login", {
-            emailId,
-            password,
-            },
-            {withCredentials : true}
-        );
+      // console.log(res.data); //res.data contains the actual data i.e, user data which we sent through api in backend => store this data in redux store => useDispatch hook
+      dispatch(addUser(res.data));
 
-        // console.log(res.data); //res.data contains the actual data i.e, user data which we sent through api in backend => store this data in redux store => useDispatch hook
-        dispatch(addUser(res.data));
-
-        return navigate("/");//navigate to this path after clicking login
-        } catch(err) {
-            console.error(err);
-        }
-    };
+      return navigate('/'); //navigate to this path after clicking login
+    } catch (err) {
+      setError(err?.response?.data || 'Something Went Wrong');
+      console.error(err);
+    }
+  };
 
   return (
     <div className="flex justify-center my-10">
       <div className="card bg-base-300 w-96 shadow-sm">
         <div className="card-body">
           <h1 className="card-title flex justify-center my-2">Login</h1>
-          <label className="input validator">
+          <label className="input">
             <svg
               className="h-[1em] opacity-50"
               xmlns="http://www.w3.org/2000/svg"
@@ -52,62 +55,69 @@ const Login = () => {
                 <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
               </g>
             </svg>
-            <input 
-            type="email" 
-            placeholder="mail@site.com" 
-            required 
-            value = {emailId}
-            onChange={(e) => setEmailId(e.target.value)}/>
+            <input
+              type="email"
+              placeholder="mail@site.com"
+              required
+              value={emailId}
+              onChange={e => {
+                setEmailId(e.target.value);
+                setError("");// Clear the error when user starts typing again
+              }}
+            />
           </label>
-          <div className="validator-hint hidden">Enter valid email address</div>
+          {/* <div className="validator-hint hidden">Enter valid email address</div> */}
 
           {/* <div className='my-2'> */}
-            <label className="input validator">
-              <svg
-                className="h-[1em] opacity-50"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
+          <label className="input validator">
+            <svg
+              className="h-[1em] opacity-50"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <g
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                strokeWidth="2.5"
+                fill="none"
+                stroke="currentColor"
               >
-                <g
-                  strokeLinejoin="round"
-                  strokeLinecap="round"
-                  strokeWidth="2.5"
-                  fill="none"
-                  stroke="currentColor"
-                >
-                  <path d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"></path>
-                  <circle
-                    cx="16.5"
-                    cy="7.5"
-                    r=".5"
-                    fill="currentColor"
-                  ></circle>
-                </g>
-              </svg>
-              <input
-                type="password"
-                required
-                placeholder="Password"
-                minLength="8"
-                pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
-                title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </label>
-            <p className="validator-hint hidden">
-              Must be more than 8 characters, including
-              <br />
-              At least one number
-              <br />
-              At least one lowercase letter
-              <br />
-              At least one uppercase letter
-            </p>
+                <path d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"></path>
+                <circle cx="16.5" cy="7.5" r=".5" fill="currentColor"></circle>
+              </g>
+            </svg>
+            <input
+              type="password"
+              required
+              placeholder="Password"
+              minLength="8"
+              pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+              // title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+                setError("");// Clear the error when user starts typing again
+              }}
+            />
+          </label>
+          {/* <p className="validator-hint hidden">
+            Must be more than 8 characters, including
+            <br />
+            At least one number
+            <br />
+            At least one lowercase letter
+            <br />
+            At least one uppercase letter
+          </p> */}
           {/* </div> */}
 
+          {error && (
+            <p className="text-red-500 text-center text-sm ">{error}</p>
+          )}
           <div className="card-actions justify-center my-3">
-            <button className="btn btn-primary" onClick={handleLogin}>Login</button>
+            <button className="btn btn-primary" onClick={handleLogin}>
+              Login
+            </button>
           </div>
         </div>
       </div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,34 @@
+import axios from 'axios';
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
+import { BASE_URL } from "../utils/constants"
+import { removeUser } from '../utils/userSlice';
 
 const NavBar = () => {
   const user = useSelector(store => store.user);
   // console.log(user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogout = async() => {
+    try{
+      await axios.post(BASE_URL + "/logout", 
+        {}, 
+        {withCredentials: true}
+      );
+
+      //& it should also clear user from redux store
+      dispatch(removeUser());
+
+      return navigate("/login");
+
+    }catch(err)
+    {
+      //~ Error logic => redirect to error page
+      console.error(err);
+    }
+  }
 
   return (
     <div className="navbar bg-base-300 shadow-sm">
@@ -37,14 +61,14 @@ const NavBar = () => {
               <li>
                 <Link to="/profile" className="justify-between">
                   Profile
-                  <span className="badge">New</span>
+                  {/* <span className="badge">New</span> */}
                 </Link>
               </li>
               <li>
                 <a>Settings</a>
               </li>
               <li>
-                <a>Logout</a>
+                <a onClick={handleLogout}>Logout</a>
               </li>
             </ul>
           </div>
